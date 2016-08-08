ad742c3c2d4fda669114ae0d444dfca3ad63c73a
Van Phan <VanPhan514@gmail.com>
2016-08-08 13:20:27 -0400

    Added edit pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ moves = Move.create([
 	{ name: "Hydro Pump", damage: 90, dps: 24.3, poke_type: "Water" }
 
 ])
-
+ 
 pokemons = Pokemon.create([
 	{ name: "Bulbasaur", cp: 1071, description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.", poke_type: "Grass", :moves => Move.where(:name => ['Vine Whip', 'Power Whip'])},
 	{ name: "Ivysaur", cp: 1632, description: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.", poke_type: "Grass", :moves => Move.where(:name => ['Vine Whip', 'Solar Beam'])},
