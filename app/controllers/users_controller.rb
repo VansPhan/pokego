3cfafb30d8981991f87195da0b38650ef7c78709
Van Phan <VanPhan514@gmail.com>
2016-08-10 09:47:25 -0400

    CRUD is done for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :destroy, :update, :edit]
 	def index
 		@users = User.all
 	end
@@ -6,26 +7,49 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 	def create
-		@user = User.create(create_user)
+		@user = User.create(user_params)
 		session[:user_id] = @user.id
-		redirect_to user_path(@user)
+		error "create"
 	end
 	def edit
-		
 	end
 	def update
-		
+		@user.update(user_params)
+		error "update"
 	end
 	def destroy
-		
+		@user.destroy
+		redirect_to "/users", alert: "User was successfully deleted."
 	end
 	def show
-		@user = User.find(params[:id])
+
 	end
 
 	private
 
-	def create_user
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
 		params.require(:user).permit(:email, :password, :first_name, :last_name)
+	end
+
+	def error action
+		if @user.save
+			redirect_to @user, notice: "User was successfully #{action}d."
+		else
+			message = "| "
+			@user.errors.full_messages.each do |error|
+				message << "#{error} | "
+			end
+			if action == "create"
+				redirect_to new_user_path, alert: "#{ message }"
+			elsif action == "update"
+				redirect_to edit_user_path, alert: "#{ message } | Nothing has been changed"
+			else
+				redirect_to @user, alert: "#{ message }"
+			end
+		end
 	end
 end
