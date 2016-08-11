8d9468d55bb1c8cf31a5990dd16404e9f049f2c9
Van Phan <VanPhan514@gmail.com>
2016-08-11 12:05:37 -0400

    Edited some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'pokemons#index'
-	resources :bonds, only: [:destroy]
+	resources :bonds, only: [:destroy, :create]
 	resource :session
 	resources :pokemons, :users, :moves
 end
